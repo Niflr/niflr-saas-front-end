3c181dfa46e14c8206a4451f50a4cf3487d114af
niflryash <yashraj@niflr.com>
2023-06-03 12:54:57 +0530

    added mocks

---
+++ src/_mock/videos.js
@@ -0,0 +1,38 @@
+const videos= 
+[
+        {
+            "id" : "21f5da68-73e1-4948-ba07-d217b2ad8bf9",
+            "created_at" : "2023-03-15T11:08:53.701Z",
+            "updated_at" : "2023-03-15T11:08:53.701Z",
+            "start_time" : "2023-03-15T09:15:41.055Z",
+            "end_time" : "2023-03-15T09:25:41.055Z",
+            "session_id" : "cfe9d55b-f6b7-4c37-8d01-ba9c20f04333",
+            // "video_path_local" : "dumm1\/dummy2\/dummy3",
+            // "video_path_cloud" : "dumm4\/dummy5\/dummy6",
+            "machine_id" : "1644496921479"
+        },
+        {
+            "id" : "8281240a-2d4c-4990-8326-7e101653d898",
+            "created_at" : "2023-03-15T11:09:01.112Z",
+            "updated_at" : "2023-03-15T11:09:01.112Z",
+            "start_time" : "2023-03-15T09:15:41.055Z",
+            "end_time" : "2023-03-15T09:25:41.055Z",
+            "session_id" : "cfe9d55b-f6b7-4c37-8d01-ba9c20f04333",
+            // "video_path_local" : "dumm1\/dummy2\/dummy3",
+            // "video_path_cloud" : "dumm4\/dummy5\/dummy6",
+            "machine_id" : "1644496921479"
+        },
+        {
+            "id" : "3ec12924-39fe-448f-bb34-78bb24dfc53c",
+            "created_at" : "2023-03-15T11:09:02.366Z",
+            "updated_at" : "2023-03-15T11:09:02.366Z",
+            "start_time" : "2023-03-15T09:15:41.055Z",
+            "end_time" : "2023-03-15T09:25:41.055Z",
+            "session_id" : "cfe9d55b-f6b7-4c37-8d01-ba9c20f04333",
+            // "video_path_local" : "dumm1\/dummy2\/dummy3",
+            // "video_path_cloud" : "dumm4\/dummy5\/dummy6",
+            "machine_id" : "1644496921479"
+        }
+    ]
+
+export default videos
